6ae1b117dc9daa5dd13d65c2491b270e891fc04c
ridjalfathoni <fathonirosyidi@gmail.com>
2023-01-30 01:27:43 +0700

    add homepage + sidebar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './Pages/Homepage';
+import Sidebar from './Component/Sidebar';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-      </Routes>
+      <Sidebar/>
+      <div className='content-wrapper'>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,10 +1,53 @@
 import React from 'react';
-
+import './homepage.css'
 function Homepage(props) {
     return (
-        <div>
-            <p>aa</p>
-        </div>
+        <section className='dashboard'>
+            <h1>Dashboard</h1>
+            <div className='grid-container'>
+                <div className='grid-content'>
+                    <div className='img-container'>
+                        <img src={require('../assets/icons/wallet.png')} alt="" />
+                    </div>
+                    <div className='wallet-content'>
+                        <span className='header-text'>Balance</span>
+                        <span className='content-text'>20,000.00</span>
+                    </div>
+                </div>
+                <div className='grid-content'>
+                    <div className='img-container'>
+                        <img src={require('../assets/icons/income.png')} alt="" />
+                    </div>
+                    <div className='wallet-content'>
+                        <span className='header-text'>Income</span>
+                        <span className='content-text'>20,000.00</span>
+                    </div>
+                </div>
+                <div className='grid-content'>
+                    <div className='img-container'>
+                        <img src={require('../assets/icons/expense.png')} alt="" />
+                    </div>
+                    <div className='wallet-content'>
+                        <span className='header-text'>Balance</span>
+                        <span className='content-text'>20,000.00</span>
+                    </div>
+                </div>
+                <div className='grid-content'>
+                    <div className='img-container'>
+                        <img src={require('../assets/icons/add.png')} alt="" />
+                    </div>
+                </div>
+                <div className='grid-content'>
+                    <div className='img-container'>
+                        <img src={require('../assets/icons/expense.png')} alt="" />
+                    </div>
+                    <div className='wallet-content'>
+                        <span className='header-text'>Balance</span>
+                        <span className='content-text'>20,000.00</span>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
