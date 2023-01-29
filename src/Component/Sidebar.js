8985bfe931974159f55f8f0a78c08d0754578db2
ridjalfathoni <fathonirosyidi@gmail.com>
2023-01-30 00:23:57 +0700

    add sidebar

---
+++ src/Component/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './sidebar.css';
+
+function Sidebar(props) {
+    return (
+        <>
+            <nav className='sidebar'>
+                <Link to="/">Dashboard</Link>
+                <Link to="/Transactions">Transactions</Link>
+                <Link to="/Wallet">Wallet</Link>
+            </nav>
+        </>
+    );
+}
+
+export default Sidebar;
