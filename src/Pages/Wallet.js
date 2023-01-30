a3fdd571a0bf151a052eb90d95146b17c6a903c5
ridjalfathoni <fathonirosyidi@gmail.com>
2023-01-30 20:08:38 +0700

    add Wallet Page

---
+++ src/Pages/Wallet.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Wallet(props) {
+    return (
+        <>
+            <div className="features-menu">
+                <div className="features-header">
+                    <h1>Wallet</h1>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Wallet;
