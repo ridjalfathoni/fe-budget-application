14e8298fbf9c7dcccf3bb97a2cce855b79ff82be
ridjalfathoni <fathonirosyidi@gmail.com>
2023-01-30 20:08:29 +0700

    add Transaction Page

---
+++ src/Pages/Transactions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Transactions(props) {
+    return (
+        <>
+            <div className="features-menu">
+                <div className="features-header">
+                    <h1>Transactions</h1>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Transactions;
