71f07ab7a7abd5a45dfd7725e5bd262c030c7c55
ridjalfathoni <fathonirosyidi@gmail.com>
2023-01-31 06:07:34 +0700

    edit modal

--- src/Component/Modal.js
+++ src/Component/Modal.js
@@ -2,24 +2,26 @@ import React, { useState } from 'react';
 import './modal.css'
 function Modal(props) {
     const [value, setValue] = useState({});
-    const func = (e) => {
-        console.log("child function",e.target.value);
-      };
     return (
         <div className='modal-container'>
             <div className="modal-header">
-                <h1>{props.title}</h1>
+                <h1>{props.formList.title}</h1>
                 <span onClick={() => props.onCancel()}>X</span>
             </div>
             <div className="modal-content">
                 {
-                    props.isForm &&
-                    <form onSubmit={(e) => props.submitForm(e)} className='modal-form'>
+                    props.formList.isForm &&
+                    <form onSubmit={(e) => props.submitForm(e,props.formList.isUpdate)} className='modal-form'>
                         {
-                            props.formList.map((form) => (
+                            props.formList.data.map((form) => (
                                 <div key={form.key} className="modal-form-input">
                                     <label htmlFor={form.key}>{form.label}</label>
-                                    <input type={form.type} placeholder={form.key} onChange={(e) => form.func(e)}/>
+                                    <input
+                                        name={form.key}
+                                        type={form.type}
+                                        placeholder={form.key}
+                                        onChange={form.handleUpdateChange}
+                                        defaultValue={form.value}/>
                                 </div>
                             ))
                         }
