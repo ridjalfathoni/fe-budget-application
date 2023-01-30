bf66f61d2be8d3db676fdb4ea68c0745fb49d427
ridjalfathoni <fathonirosyidi@gmail.com>
2023-01-30 13:36:23 +0700

    slicing design

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,53 +1,79 @@
 import React from 'react';
-import './homepage.css'
+// import './homepage.css'
+
 function Homepage(props) {
     return (
-        <section className='dashboard'>
+        <section className='content'>
             <h1>Dashboard</h1>
-            <div className='grid-container'>
-                <div className='grid-content'>
-                    <div className='img-container'>
-                        <img src={require('../assets/icons/wallet.png')} alt="" />
-                    </div>
-                    <div className='wallet-content'>
-                        <span className='header-text'>Balance</span>
-                        <span className='content-text'>20,000.00</span>
-                    </div>
-                </div>
-                <div className='grid-content'>
-                    <div className='img-container'>
-                        <img src={require('../assets/icons/income.png')} alt="" />
-                    </div>
-                    <div className='wallet-content'>
-                        <span className='header-text'>Income</span>
-                        <span className='content-text'>20,000.00</span>
-                    </div>
-                </div>
-                <div className='grid-content'>
-                    <div className='img-container'>
-                        <img src={require('../assets/icons/expense.png')} alt="" />
-                    </div>
-                    <div className='wallet-content'>
-                        <span className='header-text'>Balance</span>
-                        <span className='content-text'>20,000.00</span>
-                    </div>
-                </div>
-                <div className='grid-content'>
-                    <div className='img-container'>
-                        <img src={require('../assets/icons/add.png')} alt="" />
-                    </div>
-                </div>
-                <div className='grid-content'>
-                    <div className='img-container'>
-                        <img src={require('../assets/icons/expense.png')} alt="" />
-                    </div>
-                    <div className='wallet-content'>
-                        <span className='header-text'>Balance</span>
-                        <span className='content-text'>20,000.00</span>
-                    </div>
-                </div>
-            </div>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
+            <h1>Dashboard</h1>
         </section>
+        // <section className='content'>
+        //     <div className='header-content'>
+        //         <h1>Dashboard</h1>
+        //     </div>
+        //     <div className='balance-container'>
+        //         <div className='img-container'>
+        //             <img src={require('../assets/icons/wallet.png')} alt="" />
+        //         </div>
+        //         <div className='wallet-content'>
+        //             <span className=''>Balance</span>
+        //             <span className='content-text'>20,000.00</span>
+        //         </div>
+        //     </div>
+        //     <div className='income-container'>
+        //         <div className='img-container'>
+        //             <img src={require('../assets/icons/income.png')} alt="" />
+        //         </div>
+        //         <div className='wallet-container'>
+        //             <span className=''>Income</span>
+        //             <span className='content-text'>20,000.00</span>
+        //         </div>
+        //     </div>
+        //     <div className='expense-container'>
+        //         <div className='img-container'>
+        //             <img src={require('../assets/icons/expense.png')} alt="" />
+        //         </div>
+        //         <div className='wallet-container'>
+        //             <span className=''>Expense</span>
+        //             <span className='content-text'>20,000.00</span>
+        //         </div>
+        //     </div>
+        //     <div className='add-container'>
+        //         <div className='img-container'>
+        //             <img src={require('../assets/icons/add.png')} alt="" />
+        //         </div>
+        //     </div>
+        //     <div className='aa-container'>
+        //         <div className='img-container'>
+        //             <img src={require('../assets/icons/add.png')} alt="" />
+        //         </div>
+        //     </div>
+        //     <div className='aa-container'>
+        //         <div className='img-container'>
+        //             <img src={require('../assets/icons/add.png')} alt="" />
+        //         </div>
+        //     </div>
+        //     <div className='aa-container'>
+        //         <div className='img-container'>
+        //             <img src={require('../assets/icons/add.png')} alt="" />
+        //         </div>
+        //     </div>
+        //     <div className='aa-container'>
+        //         <div className='img-container'>
+        //             <img src={require('../assets/icons/add.png')} alt="" />
+        //         </div>
+        //     </div>
+        // </section>
     );
 }
 
