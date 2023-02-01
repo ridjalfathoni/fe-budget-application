f75ac653ec074c843adf0a3d9088c37eb5339f76
ridjalfathoni <fathonirosyidi@gmail.com>
2023-02-01 23:38:52 +0700

    dashboard card

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import Card from '../Component/Card';
+import List from '../Component/List';
 // import './Dashboard.css'
 
 function Dashboard(props) {
+
+    const dummyCard = [
+        {
+            key: "balance",
+            subheader: "Balance",
+            amount: 10000,
+            imgUrl: require('../assets/icons/wallet.png')
+        },
+        {
+            key: "income",
+            subheader: "Income",
+            amount: 10000,
+            imgUrl: require('../assets/icons/income.png')
+        },
+        {
+            key: "expense",
+            subheader: "Expense",
+            amount: 10000,
+            imgUrl: require('../assets/icons/expense.png')
+        },
+        {
+            key: "add",
+            imgUrl: require('../assets/icons/add.png')
+        },
+    ]
+
     return (
         <>
             <div className="features-menu">
@@ -9,138 +37,8 @@ function Dashboard(props) {
                     <h1>Dashboard</h1>
                 </div>
             </div>
-            {/* <div className="balance items-container">
-                <div className="items-img">
-                    <img src={require("../assets/icons/wallet.png")} alt="" />
-                </div>
-                <div className="items-content">
-                    <span className='text-name'>Balance</span>
-                    <span className='text-amount'>20,000.00</span>
-                </div>
-            </div>
-            <div className="income items-container">
-                <div className="items-img">
-                    <img src={require("../assets/icons/income.png")} alt="" />
-                </div>
-                <div className="items-content">
-                    <span className='text-name'>Income</span>
-                    <span className='text-amount'>20,000.00</span>
-                </div>
-            </div>
-            <div className="expense items-container">
-                <div className="items-img">
-                    <img src={require("../assets/icons/expense.png")} alt="" />
-                </div>
-                <div className="items-content">
-                    <span className='text-name'>Expense</span>
-                    <span className='text-amount'>20,000.00</span>
-                </div>
-            </div>
-            <div className="add items-container">
-                <div className="items-img">
-                    <img src={require("../assets/icons/add.png")} alt="" />
-                </div>
-            </div>
-            <div className="overview items-container">
-                <div className="overview-header">
-                    <span className='text-name'>Overview</span>
-                    <select name="date" id="date" className='overview-select'>
-                        <option value="monthly" defaultValue={true}> Monthly</option>
-                        <option value="weekly"> Weekly</option>
-                    </select>
-                </div>
-            </div>
-            <div className="recent-transactions items-container">
-                <div className="recent-transactions-header">
-                    <span className='text-name'>Recent Transactions</span>
-                </div>
-                <div className="recent-transactions-content">
-                    <div className="recent-transaction-items">
-                        <div className="recent-img-container">
-                            <img src={require("../assets/icons/add.png")} alt="" />
-                        </div>
-                        <div className="content-wrapper">
-                            <div>
-                                <span className='text-category'>Salary</span>
-                                <span className='text-date'>28 January 2023</span>
-                            </div>
-                            <span className='text-amount-income'>1000000</span>
-                        </div>
-                    </div>
-                    <div className="recent-transaction-items">
-                        <div className="recent-img-container">
-                            <img src={require("../assets/icons/add.png")} alt="" />
-                        </div>
-                        <div className="content-wrapper">
-                            <div>
-                                <span className='text-category'>Salary</span>
-                                <span className='text-date'>28 January 2023</span>
-                            </div>
-                            <span className='text-amount-expense'>1000000</span>
-                        </div>
-                    </div>
-                    <div className="recent-transaction-items">
-                        <div className="recent-img-container">
-                            <img src={require("../assets/icons/add.png")} alt="" />
-                        </div>
-                        <div className="content-wrapper">
-                            <div>
-                                <span className='text-category'>Salary</span>
-                                <span className='text-date'>28 January 2023</span>
-                            </div>
-                            <span className='text-amount-expense'>1000000</span>
-                        </div>
-                    </div>
-                    <div className="recent-transaction-items">
-                        <div className="recent-img-container">
-                            <img src={require("../assets/icons/add.png")} alt="" />
-                        </div>
-                        <div className="content-wrapper">
-                            <div>
-                                <span className='text-category'>Salary</span>
-                                <span className='text-date'>28 January 2023</span>
-                            </div>
-                            <span className='text-amount-expense'>1000000</span>
-                        </div>
-                    </div>
-                    <div className="recent-transaction-items">
-                        <div className="recent-img-container">
-                            <img src={require("../assets/icons/add.png")} alt="" />
-                        </div>
-                        <div className="content-wrapper">
-                            <div>
-                                <span className='text-category'>Salary</span>
-                                <span className='text-date'>28 January 2023</span>
-                            </div>
-                            <span className='text-amount-expense'>1000000</span>
-                        </div>
-                    </div>
-                    <div className="recent-transaction-items">
-                        <div className="recent-img-container">
-                            <img src={require("../assets/icons/add.png")} alt="" />
-                        </div>
-                        <div className="content-wrapper">
-                            <div>
-                                <span className='text-category'>Salary</span>
-                                <span className='text-date'>28 January 2023</span>
-                            </div>
-                            <span className='text-amount-income'>1000000</span>
-                        </div>
-                    </div>
-                    <div className="recent-transaction-items">
-                        <div className="recent-img-container">
-                            <img src={require("../assets/icons/add.png")} alt="" />
-                        </div>
-                        <div className="content-wrapper">
-                            <div>
-                                <span className='text-category'>Salary</span>
-                                <span className='text-date'>28 January 2023</span>
-                            </div>
-                            <span className='text-amount-expense'>1000000</span>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+            <Card cardListContent={dummyCard} />
+            <List title="Recent Transactions" data={[]}/>
         </>
     );
 }
